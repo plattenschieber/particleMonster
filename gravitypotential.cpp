4b77ba19fe3517ae6c96b01ef596d215a646c995
Platten Schieber <plattenschieber@gmail.com>
2011-11-26 15:26:30 +0100

    again some commenting and editing stuff

--- gravitypotential.cpp
+++ gravitypotential.cpp
@@ -10,10 +10,11 @@ real GravityPotential::force(Particle &p, Particle &q)
     // we don't need no sqrt to exponentiate r by 3 
     real r3 = r2 * sqrt(r2);
     // scalar factor of our directional force-vector (induced by scaled gravitypotential)
-	real scal  = -p.m*q.m/r3;
-	for (int d=0; d<DIM; d++)
-		p.F[d] += -scal * (q.x[d]-p.x[d]);
-	return scal;
+    real scal  = -p.m*q.m/r3;
+    // add force to our particle p
+    for (int d=0; d<DIM; d++)
+	p.F[d] += -scal * (q.x[d]-p.x[d]);
+    return scal;
 }
 
 // vim:set et sts=4 ts=4 sw=4 ai ci cin:
