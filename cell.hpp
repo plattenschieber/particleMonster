143be69e9a2d717830b3c704889c3ed0e8581241
Platten Schieber <plattenschieber@gmail.com>
2011-11-26 19:39:10 +0100

    add cell class

---
+++ cell.hpp
@@ -0,0 +1,19 @@
+#ifndef _CELL_HPP
+#define _CELL_HPP
+
+#include "defines.hpp"
+#include <vector>
+
+/**
+ * @brief Particle data
+ *
+ * this class contains the particle data
+ */
+class Cell {
+public:
+    /// The cell contains particles ...
+    //std::vector<Particle> particles;
+};
+
+#endif // _CELL_HPP
+// vim:set et sts=4 ts=4 sw=4 ai ci cin:
