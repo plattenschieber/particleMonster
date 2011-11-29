6f949de4a971b0e969708260d9683c00f0e2a9aa
Platten Schieber <plattenschieber@gmail.com>
2011-11-30 00:43:09 +0100

    first approach compF

--- velocityverletlc.cpp
+++ velocityverletlc.cpp
@@ -19,21 +19,33 @@ void VelocityVerletLC::comp_F()
     real dist = 0.0, rcut = 2.5;
     // there is no e_pot in the beginning
     W.e_pot = 0.0;
-    // we compute the e_pot for each pair of particles and add it to the worlds' e_pot...
-    for (std::vector<Particle>::iterator i = W.particles.begin(); i < W.particles.end(); i++)
-        // ...except of the computation with itself (i!=j)
-	for (std::vector<Particle>::iterator j = W.particles.begin(); j < i; j++) 
+    // roll over each cell
+    for (std::vector<Cell>::iterator cell = W.cells.begin(); cell < W.cells.end(); cell++)
+        // we compute the e_pot for each pair of particles in it's cell including the neighbour cells and add it to the worlds' e_pot...
+	// roll over every particle i in actual cell
+        for (std::vector<Particle>::iterator i = cell->particles.begin(); i < cell->particles.end(); i++)
 	{
-		// don't forget to reset the distance
-		dist = 0.0;
-		// check the distance
-    		for (int d=0; d<DIM; d++)
-		    dist += sqr(j->x[d]-i->x[d]);	
-		    // only particles which are closer than rcut
-		    if(dist <= rcut*999) 
-		         // computes the force between particle i and j and add it to our potential
-			 W.e_pot += Pot.force(*i, *j);
-		}
+	    // set all F_i to zero
+	    for (int d=0; d<DIM; d++) i->F[d]=0;
+	    // roll over every neighbour cell
+	    for (std::vector<Cell>::iterator neighbour = cell+1; neighbour += 2)    
+                // ...except of the computation with itself (i!=j)
+	        for (std::vector<Particle>::iterator j = neighbour->particles.begin(); j < neighbour->particles.end(); j++) 
+	        {
+			if(i!=j)
+			{
+			    // don't forget to reset the distance
+			    dist = 0.0;
+			    // check the distance
+			    for (int d=0; d<DIM; d++) dist += sqr(j->x[d] - i->x[d]);	
+				// only particles which are closer than rcut
+				if(dist <= rcut) 
+				     // computes the force between particle i and j and add it to our potential
+				     W.e_pot += Pot.force(*i, *j);
+			}
+	       }
+	}
+    
 }
 
 void VelocityVerletLC::update_V()
