0b8b6617c301339736fb8026b2b6d3ad8ff70f99
Platten Schieber <plattenschieber@gmail.com>
2011-12-10 02:43:10 +0100

    important change to read_Parameter and read_Particles
    add ability to compute cell length and other for the linked cell
    algorithm needed attributes (and insert #cells empty cells) on
    read_Parameter
    
    read_Particles pushes every particle in it's belonging cell by means of
    getCellNumber()

--- worldlc.cpp
+++ worldlc.cpp
@@ -1,5 +1,6 @@
 #include "worldlc.hpp"
 #include "world.hpp"
+#include "cell.hpp"
 #include <stdexcept>
 #include <sstream>
 #include <map>
@@ -44,23 +45,65 @@ void WorldLC::read_Parameter(const std::string &filename)
     // close file
     parfile.close();
     
-    
+    // #of all cells
+    int nCells = 1;
+
     for (int d=0; d<DIM; d++) 
+    {
+        std::cout << std::endl << "length: " << length[d] << "cell r cut: " << cell_r_cut << std::endl;
         // #cells in dimension = floor(length per cell-cutlength)
         cell_N[d] = (int)(length[d]/cell_r_cut);
+        // cell length = world length per #cells
+        cell_length[d] = length[d]/cell_N[d];
 
-    system("pause");
-    for (int d=0; d<cell_N[d]; d++);
-    //cells.push_back(NULL);
+        nCells *= cell_N[d];
+        // DEBUG
+        std::cout << "#Cell: " << cell_N[d] << "\tCelllength: " << cell_length[d] << std::endl;
+    }
+    // DEBUG 
+    std::cout << "#Cells: " << nCells << std::endl;
 
+    // hold on computer and give us all what you have! 
+    std::cin.clear(); 
+    std::cin.ignore(INT_MAX, '\n');  
+    std::cout << "Press Return..." << std::endl;  
+    std::cin.get();
+
+
+    
+    
+    // insert empty cells
+    for (int i=0; i<nCells; i++);
+        cells.push_back(Cell());
 }
 
 void WorldLC::read_Particles(const std::string &filename)
 {
     // call the base function
     World::read_Particles(filename);
-    // TODO: Write every particle into it's cell
-    //for 
-}
+    // Write every particle into it's belonging cell
+    for (std::vector<Particle>::iterator i = particles.begin(); i < particles.end(); i++)
+    {
+        // add particle to right cell...
+        // getCellNumber(i) gives belonging cellnumber, push into this cell our actual particle i: particles[i-particles.begin()]
+        cells[getCellNumber(i)].particles.push_back(particles[i-particles.begin()]);
+            //particles.push_back(i);
+        // ...and remove it from our particle list
+        particles.erase(i);
 
+    }
+}
+int WorldLC::getCellNumber(const std::vector<Particle>::iterator i) 
+{
+    int tmp[3] = {0,0,0};
+    for (int d=0; d<DIM; d++)
+    {
+        tmp[d] = i->x[d] * cell_N[d] / length[d];
+        // DEBUG
+        std::cout << tmp[d] << "\tx[" << d << "]: " << i->x[d] << "\t "<< cell_N[d] / length[d] << std::endl;
+    }
+    std::cout << "Corresponding Index: " << J(tmp,cell_N) << std::endl;
+    return J(tmp,cell_N);
+
+}
 // vim:set et sts=4 ts=4 sw=4 ai ci cin:
