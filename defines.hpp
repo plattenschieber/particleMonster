cbcd74c59275c031bda79dc5e3d319d8591c2db1
Platten Schieber <plattenschieber@gmail.com>
2011-12-10 02:34:48 +0100

    move index expand macro to defines for everywhere use

--- defines.hpp
+++ defines.hpp
@@ -32,5 +32,12 @@ typedef double real;
 // squre define
 #define sqr(_x) ((_x)*(_x))
 
+// small macro, to expand index calculation for different dimensions
+#if DIM == 2
+#define J(jCell,cell_N) ((jCell)[1] + (cell_N)[1]*(jCell)[0]) 
+#elif DIM == 3
+#define J(jCell,cell_N) ((jCell)[2] + (cell_N)[2]*((jCell[1] + (cell_N)[1]*(jCell)[0])))
+#endif
+
 #endif // _DEFINES_HPP
 // vim:set et sts=4 ts=4 sw=4 ai ci cin cino=g0,t0:
