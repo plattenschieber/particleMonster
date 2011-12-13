b11a3b533e87779283ecb4fdd91295f94ea3abeb
Platten Schieber <plattenschieber@gmail.com>
2011-12-13 15:51:26 +0100

    remove simple force without sigma, eps;

--- ljpotential.cpp
+++ ljpotential.cpp
@@ -1,25 +1,6 @@
 #include "ljpotential.hpp"
 #include <math.h>
 #include <iostream>
-real LJPotential::force(Particle &p, Particle &q)
-{
-    // potential depth and zero breakthrough
-    real eps = 1.0, sigma = 1.0, dist = 0.0;
-
-    for(int d=0; d<DIM; d++)
-	    dist+= sqr(q.x[d] - p.x[d]);
-    
-    // potential between p and q
-    real Urij = 4*eps*pow(sqr(sigma)/dist,3)*(pow(sqr(sigma)/dist,3) - 1);
-    
-    // force is transfered from q to p by means of ljpotential
-    for (int d=0; d<DIM; d++) {
-	double force = 24*eps*(1/dist)*pow(sigma*sigma/dist,3)*(1-2*pow(sigma*sigma/dist,3))*(q.x[d]-p.x[d]);
-	p.F[d] += force;
-	q.F[d] -= force;
-    }
-    return Urij;
-}
 real LJPotential::force(Particle &p, Particle &q, real dist , real eps, real sigma)
 {
     // potential depth and zero breakthrough
