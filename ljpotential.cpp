2fd4fe531129e786782bf2e2b3b8bbb11988987a
Platten Schieber <plattenschieber@gmail.com>
2012-01-11 10:50:46 +0100

    we not only need the distance but a directional vector, too

--- ljpotential.cpp
+++ ljpotential.cpp
@@ -1,20 +1,20 @@
 #include "ljpotential.hpp"
 #include <cmath>
 #include <iostream>
-real LJPotential::force(Particle &p, Particle &q, real dist , real eps, real sigma)
+real LJPotential::force(Particle &p, Particle &q, real dist, real distV[], real eps, real sigma)
 {
     // potential depth and zero breakthrough
     //real eps = 1.0, sigma = 1.0;
-    
+
     // potential between p and q
     real Urij = 4*eps*pow(sqr(sigma)/dist,3)*(pow(sqr(sigma)/dist,3) - 1);
     
     // force is transfered from q to p by means of ljpotential
     for (int d=0; d<DIM; d++) {
-	double force = 24*eps*(1/dist)*pow(sigma*sigma/dist,3)*(1-2*pow(sigma*sigma/dist,3))*(q.x[d]-p.x[d]);
-	p.F[d] += force;
-	// do we really can already pull it off? NO!
-    q.F[d] -= force;
+        real force = 24*eps*(1/dist)*pow(sigma*sigma/dist,3)*(1-2*pow(sigma*sigma/dist,3))*(distV[d]);
+        p.F[d] += force;
+        // do we really can already pull it off? NO!
+        q.F[d] -= force;
     }
     return Urij;
 }
