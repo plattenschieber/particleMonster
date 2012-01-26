55471f00e8f87848ad7d1d755ac1b8111dd1143e
Platten Schieber <plattenschieber@gmail.com>
2012-01-26 16:08:46 +0100

    just moved row

--- worldlc.hpp
+++ worldlc.hpp
@@ -38,13 +38,13 @@ public:
                               int upper_proc, int *upper_ic_start,  int *upper_ic_stop, int *upper_ic_startreceive, int *upper_ic_stopreceive);
     int getCellNumber (const Particle &p);
 
-    void construct_particle(MPI::Datatype& MPI_Particle);
 
-    void SetCommunication(SubDomain *s, int d,
-                                int *lower_ic_start, int *lower_ic_stop, int *lower_ic_startreceive, int *lower_ic_stopreceive,
-                                int *upper_ic_start, int *upper_ic_stop, int *upper_ic_startreceive, int *upper_ic_stopreceive);
+    void construct_particle (MPI::Datatype& MPI_Particle);
 
     void Communication (Cell *grid, SubDomain *s, bool isForward);
+    void setCommunication ( int d,
+                            int *lower_ic_start, int *lower_ic_stop, int *lower_ic_startreceive, int *lower_ic_stopreceive,
+                            int *upper_ic_start, int *upper_ic_stop, int *upper_ic_startreceive, int *upper_ic_stopreceive);
 
     // Value-Defintions of the different String values
     // needed to be implemented again, because enum is not extandable
