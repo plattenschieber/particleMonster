b90966d5a2372324dc43a93acdf242d00e5d92e6
Plattenschieber <plattenschieber@gmail.com>
2012-02-01 11:02:20 +0100

    small changes

--- observerxyz.cpp
+++ observerxyz.cpp
@@ -19,28 +19,26 @@ ObserverXYZ::~ObserverXYZ()
     // close the coordinates file
     if ( coordinates.is_open() )
         coordinates.close();
-    std::cout << "Everything closed properly" << std::endl;
+    std::cout << "Everything closed properly in ObserverXYZ" << std::endl;
 }
 
 void ObserverXYZ::outputCoordinates ()
 {
     // write size and actual time of our world W
-    xyz << W.nParticles << std::endl << "Time: " << W.t << std::endl;
+    coordinates << W.nParticles << std::endl << "Time: " << W.t << std::endl;
     // get out every particle to satisfy the xyz format
     for (std::vector<Cell>::const_iterator i = W.cells.begin(); i < W.cells.end(); i++)
-    {
         for (std::list<Particle>::const_iterator j = i->particles.begin(); j != i->particles.end(); j++)
         {
             // each particle should be an H-atom. At least now...
-            xyz << "H\t";
+            coordinates << "H\t";
             // particle j is located in a DIM-dimensional space
             for (unsigned int d=0; d<DIM; d++)
                 // get it out, seperated with tabulars
-                xyz << j->x[d] << "\t";
+                coordinates << j->x[d] << "\t";
             // newline at end of each particle
-            xyz << std::endl;
+            coordinates << std::endl;
         }
-    }
 }
 
 void ObserverXYZ::notify()
@@ -48,7 +46,4 @@ void ObserverXYZ::notify()
     outputCoordinates ();
     outputStatistics ();
 }
-
-
-
 // vim:set et sts=4 ts=4 sw=4 ai ci cin:
