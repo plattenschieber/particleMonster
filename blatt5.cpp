7a76b67ed5bfa6c971de868bd499eceb7b07d240
Plattenschieber <plattenschieber@gmail.com>
2012-02-09 19:51:39 +0100

    SubDomain and parallel Observer are acting now

--- blatt5.cpp
+++ blatt5.cpp
@@ -5,6 +5,7 @@
 #include <mpi.h>
 
 
+#include "parobserverxyz.hpp"
 #include "world.hpp"
 #include "worldlc.hpp"
 #include "gravitypotential.hpp"
@@ -34,25 +35,25 @@ int main(int argc, char *argv[]) {
     LJPotential LPot;
 
     // create World
-    WorldLC W;
+    SubDomain S;
     // debub propose, show 2nd argument
     std::cout << argv[2] << std::endl;
 
     // read Parameters
-    W.readParameter(argv[1]);
+    S.readParameter(argv[1]);
 
     // read Particles
-    W.readParticles(argv[2]);
+    S.readParticles(argv[2]);
 
     // print World configuration after Building it up
-    std::cout << W << std::endl;
+    std::cout << S << std::endl;
 
     // create the Observer
-    ObserverXYZ O(W);
+    ParObserverXYZ O(S);
 
     // instanciate timediscretization
     // remark: & is used to get the address of Pot // reremark: removed the &
-    VelocityVerletLC verletLC(W, LPot, O);
+    VelocityVerletLC verletLC(S, LPot, O);
 
     // run the simulation
     verletLC.simulate();
