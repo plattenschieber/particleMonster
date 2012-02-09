76c7922e99ad4ae9f74e179a69e56b9dc5430736
Plattenschieber <plattenschieber@gmail.com>
2012-02-09 20:43:48 +0100

    introduce the parallel observer
    controlls output filenames and assigns statistic output to first proc

---
+++ parobserverxyz.hpp
@@ -0,0 +1,49 @@
+#ifndef _PAROBSERVERXYZ_HPP
+#define _PAROBSERVERXYZ_HPP
+
+#include "subdomain.hpp"
+#include "observerxyz.hpp"
+#include <iostream>
+#include <fstream>
+
+/**
+ * @brief This Observer will controll the coordinates filenames and limit statistics output to first proc
+ */
+class ParObserverXYZ : public ObserverXYZ  {
+public:
+    /**
+     * @brief constructor
+     *
+     * opens and creates the files written during observation
+     *
+     * @param _W
+     */
+    ParObserverXYZ(SubDomain& _S);
+
+    /**
+     * @brief destructor
+     *
+     * closes the files written during the obervation
+     */
+    ~ParObserverXYZ();
+
+    /*
+    * @brief notify the observer that the world has changed
+    */
+    virtual void notify ();
+
+
+    
+protected:
+    /// The world we are observing
+    SubDomain &S;
+    /// coordiantes filestream
+    std::ofstream coordinates;
+
+private:
+    /// Disabled Standard Constructor
+    ParObserverXYZ();
+};
+
+#endif // _PAROBSERVERXYZ_HPP
+// vim:set et sts=4 ts=4 sw=4 ai ci cin:
