d7dfd7dcddb0083bad06bc7ed3c4c9e655c99ffd
Plattenschieber <plattenschieber@gmail.com>
2012-02-12 22:10:02 +0100

    FIXBUG #particles in observer

--- observerxyz.cpp
+++ observerxyz.cpp
@@ -25,21 +25,26 @@ ObserverXYZ::~ObserverXYZ()
 
 void ObserverXYZ::outputCoordinates ()
 {
+    int nParticles = 0;
+    std::stringstream str;
     // write size and actual time of our world W
-    coordinates << W.nParticles << std::endl << "Time: " << W.t << std::endl;
+    //coordinates << W.nParticles << std::endl << "Time: " << W.t << std::endl;
     // get out every particle to satisfy the xyz format
     for (std::vector<Cell>::const_iterator i = W.cells.begin(); i < W.cells.end(); i++)
         for (std::list<Particle>::const_iterator j = i->particles.begin(); j != i->particles.end(); j++)
         {
+            nParticles++;
             // each particle should be an H-atom. At least now...
-            coordinates << "H\t";
+            str << "H\t";
             // particle j is located in a DIM-dimensional space
             for (unsigned int d=0; d<DIM; d++)
                 // get it out, seperated with tabulars
-                coordinates << j->x[d] << "\t";
+                str << j->x[d] << "\t";
             // newline at end of each particle
-            coordinates << std::endl;
+            str << std::endl;
         }
+
+    coordinates << nParticles << std::endl << "Time: " << W.t << std::endl << str;
 }
 
 void ObserverXYZ::notify()
